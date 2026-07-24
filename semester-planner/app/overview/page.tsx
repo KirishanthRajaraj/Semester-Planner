165890d63467fd3ba3dd306ec40732fddea65f02
Kiri Biri <kiribiri@Kirishanths-MacBook-Air.local>
2026-07-24 12:38:26 +0200

    created recursive function to determine progress of level 1 tasks

--- semester-planner/app/overview/page.tsx
+++ semester-planner/app/overview/page.tsx
@@ -1,17 +1,72 @@
 "use client"
 import { TasksTable } from "@/components/TasksTable";
+import { Progress } from "@/components/ui/progress";
 import { TaskItem } from "@/interfaces/taskItem";
+import { getChildren } from "@/lib/taskOperations";
 import { useTaskStore } from "@/store/taskStore";
+import { useEffect, useState } from "react";
 
 
 export default function Overview() {
 
     const tasks = useTaskStore((state) => state.tasks);
+    const [tasksProgress, setTaskProgress] = useState<{ task: TaskItem, progress: number }[]>([]);
+    let topTasks = tasks.filter((task) => task.parentId == undefined);
+    let amountDone: number;
+    let amountTop: number;
+    let tempPercentage: number = 0;
+
+    useEffect(() => {
+        // getprogress function only appends, so the array needs to be cleared before function recall
+        setTaskProgress([]);
+        getProgressOfAllTopTasks(topTasks);
+    }, [tasks]);
+
+    useEffect(() => {
+        console.log(tasksProgress)
+    }, [tasksProgress]);
+
+    const getProgressOfAllTopTasks = (topTasks: TaskItem[]) => {
+        topTasks.forEach((topTask) => {
+            let topTChildren = getChildren(tasks, topTask.id);
+            if (topTChildren.length > 0) {
+                amountDone = topTChildren.filter((task) => task.status === "done").length;
+                amountTop = topTChildren.length;
+                tempPercentage += (1 / topTasks.length) * (amountDone / amountTop);
+                getProgressOfAllTopTasks(topTChildren);
+                //check if we reached level 1 task items
+                if (topTask.parentId == undefined) {
+                    // weird react state setting timing bug, do not remove
+                    const progress = tempPercentage;
+                    setTaskProgress(prev => [...prev, { task: topTask, progress: progress }])
+                    tempPercentage = 0;
+                }
+            } else {
+                // base condition:
+                return;
+            }
+        });
+    }
 
     return (
         <>
-            <h1 className="text-3xl font-bold mb-12 mt-16">Übersicht</h1>
-            <TasksTable />
+            <div className="container font-sans flex flex-col flex-1 justify-center">
+
+                <h1 className="text-3xl font-bold mb-12 mt-16">Übersicht</h1>
+                <TasksTable />
+
+                <div className="flex flex-col gap-4 w-full">
+                    <h2 className="text-xl font-bold mb-4 mt-16">Progress</h2>
+                    {tasksProgress.map((task) => {
+                        return (
+                            <div key={task.task.id}>
+                                <p className="font-bold mb-2">{task.task.title}</p>
+                                <Progress value={(task.progress) * 100} className="w-[40%]" />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </>
     );
 }
--- semester-planner/lib/taskOperations.ts
+++ semester-planner/lib/taskOperations.ts
@@ -2,3 +2,7 @@ import { TaskItem } from "@/interfaces/taskItem";
 
 export const isTaskOverdue = (task: TaskItem): boolean =>
     !!task.date && task.date < new Date();
+
+// get direct children of a task
+export const getChildren = (tasks: TaskItem[], parentId: string): TaskItem[] =>
+    tasks.filter((t) => t.parentId === parentId);
